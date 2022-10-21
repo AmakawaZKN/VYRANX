cf599a6ac0e2320e3274a3821c1b60f0574bec61
AmakawaZKN <108383788+AmakawaZKN@users.noreply.github.com>
2022-10-21 20:24:35 +0700

    Update hallofangz.js

--- plugins/hallofangz.js
+++ plugins/hallofangz.js
@@ -2,9 +2,9 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
 `.trim()
 
-conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*✧───────···────────✧*\n*Hallo Juga Kak Shinn Kangen Nih* \n *Gimana Kabar Nya? >~<*', 'status@broadcast')
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*✧───────···────────✧*\n*Hallo Juga Kak Vyranx Kangen Nih* \n *Gimana Kabar Nya? >~<*', 'status@broadcast')
 }
-handler.customPrefix = /^(Hallo Fangzz|Hallo fangz|hi fangz|Hi Fangz)$/i
+handler.customPrefix = /^(Hallo Zykuan|Hallo zyy|hi Zykuan|Hi Zyy)$/i
 handler.command = new RegExp
 
 handler.owner = false

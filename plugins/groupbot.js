98c2a34385cce0aa0295d090ae304107fddf32a5
AmakawaZKN <108383788+AmakawaZKN@users.noreply.github.com>
2022-10-21 20:32:03 +0700

    Update groupbot.js

--- plugins/groupbot.js
+++ plugins/groupbot.js
@@ -4,14 +4,13 @@ let info = `
 `
 const sections = [
    {
-	title: `𝗝𝗢𝗜𝗡 𝗞𝗘 𝗚𝗥𝗨𝗣 𝗢𝗙𝗙𝗜𝗖𝗜𝗔𝗟 𝗦𝗛𝗜𝗡𝗡 𝗕𝗢𝗧*\n\n\n━━━〔 Dᴏɴ'ᴛ ғᴏʀɢᴇᴛ Dᴏɴᴀᴛɪᴏɴ 〕━━━`,
+	title: `𝗝𝗢𝗜𝗡 𝗞𝗘 𝗚𝗥𝗨𝗣 𝗢𝗙𝗙𝗜𝗖𝗜𝗔𝗟 𝗩𝗬𝗥𝗔𝗡𝗫 𝗕𝗢𝗧*\n\n\n━━━〔 Dᴏɴ'ᴛ ғᴏʀɢᴇᴛ Dᴏɴᴀᴛɪᴏɴ 〕━━━`,
 	rows: [
-	    {title: '💌 › Group Fangz BOT', description: "Group Utama Fangz BOT", rowId:".gcbot"},
-        {title: '✉️ › Grup TokoBot', description: "Group Kedua", rowId:".gcbot2"},
+	    {title: '💌 › Group 𝗩𝗬𝗥𝗔𝗡𝗫', description: "Group Utama 𝗩𝗬𝗥𝗔𝗡𝗫", rowId:".gcbot"},
         {title: '📧 › Group Full Bot', description: "Gruop Ketiga", rowId:".gcbot3"},
-        {title: '🎐 › Owner', description: "Creator Fangz BOT >ω<", rowId:".owner"},
-        {title: '🧿 › Info Fangz BOT', description: "Info Fangz BOT >ω<", rowId:".info"},
-        {title: '📮 › Donasi', description: "Donasi Untuk Fangz ≧▽≦", rowId:".donasi"},
+        {title: '🎐 › Owner', description: "Creator 𝗭𝗬𝗞𝗨𝗔𝗡 >ω<", rowId:".owner"},
+        {title: '🧿 › Info Fangz BOT', description: "Info 𝗩𝗬𝗥𝗔𝗡𝗫 >ω<", rowId:".info"},
+        {title: '📮 › Donasi', description: "Donasi Untuk 𝗭𝗬𝗞𝗨𝗔𝗡 ≧▽≦", rowId:".donasi"},
 	    ]
       },
 ]
